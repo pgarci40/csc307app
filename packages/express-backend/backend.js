b8971849c4fa4dbae8c2458f20e6b65577b32f8c
Priscilla Garcia <pgarci40@calpoly.edu>
2025-10-08 15:47:05 -0700

    step4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,10 +1,15 @@
+
 import express from "express";
-
 const app = express();
-
 const port = 8000;
-
 app.use(express.json());
+
+const findUserByName = (name) => {
+    return users["user_list"].filter(
+        (user) => user["name"] == name
+    );
+};
+
 
 const users = {
     user_list: [
@@ -12,14 +17,32 @@ const users = {
             id: "xyz789",
             name: "Charlie", 
             job: "janitor"
+        },
+        {
+            id: "abc123",
+            name: "Mac",
+            job: "bouncer"
+        },
+        {
+            id: "ppp222",
+            name: "Mac",
+            job: "professor"
         }
     ]
 };
-app.get("/", (req, res) => {
-    res.send(users);
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}/users`);
 });
