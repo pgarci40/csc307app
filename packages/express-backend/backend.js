c94f4a83ad75ee6d3a7be4913d437fff401bdc64
Priscilla Garcia <pgarci40@calpoly.edu>
2025-10-08 22:39:07 -0700

    DELETE

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -11,6 +11,24 @@ const findUserByName = (name) => {
         (user) => user["name"] == name
     );
 };
+const findUserById = (id) =>
+    users["user_list"].find((user) =>
+    user["id"] === id);
+
+const addUser = (user) => {
+    const id = generateUserId();
+    user.id = id;
+    users["user_list"].push(user);
+    return user;
+};
+const generateUserId = () => Math.random().toString(36).slice(2, 8);
+const deleteByUserId = (id) => {
+    const idx = users["user_list"].findIndex(u => u.id === id);
+    if(idx === -1) return false;
+    users.user_list.splice(idx, 1);
+    return true;
+};
+
 const users = {
     user_list: [
         {
@@ -45,49 +63,19 @@ const users = {
         }
     ]
 };
-const findUserById = (id) =>
-    users["user_list"].find((user) =>
-    user["id"] === id);
-
-const addUser = (user) => {
-    const id = generateUserId();
-    user.id = id;
-    users["user_list"].push(user);
-    return user;
-};
-const generateUserId = () => {
-    if(!users.user_list || users.user_list.length === 0){
-        return 1;
-    }
-    const id = users.user_list.map(user => user.id).filter(id => typeof id === "number" && !isNaN(id));
-    return Math.floor(Math.random() * 1000);
-
-}
-const deleteByUserId = (id) => {
-    const idx = users["user_list"].findIndex(u => u.id === id);
-    if(idx === -1) return false;
-    users.user_list.splice(idx, 1);
-    return true;
-};
-
 app.post("/users", (req, res) => {
-    const userToAdd = req.body;
+    const userToAdd = addUser(req.body);
     const newUser = addUser(userToAdd);
 
-    if(newUser){
-        res.status(201).send();
-    }
-    else{
-        res.status(400).send("User could not be created");
-    }
+    if (newUser) return res.status(201).send(newUser);
+    return res.status(400).send("User could not be created");
 
 });
 
 app.delete("/users/:id", (req, res) => {
     const{id} = req.params;
-    if(deleteByUserId(id)){
-        return res.sendStatus(204);
-    }
+    const ok = deleteByUserId(id);
+    if (ok) return res.sendStatus(204);
     return res.status(404).send("Resource not found.");
 });
 
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -4,30 +4,60 @@ import Form from "./Form";
 function MyApp(){
   const[characters, setCharacters] = useState([
      {
+      id: "xyz789",
       name: "Charlie",
       job: "Janitor",
     },
     {
+      id: "zoo975",
       name: "Mac",
       job: "Bouncer",
     },
     {
+      id: "hsg072",
       name: "Dee",
       job: "Aspiring actress",
     },
     {
+      id: "pop625",
       name: "Dennis",
       job: "Bartender",
     }
   ]);
   function removeOneCharacter(index){
-    const updated = characters.filter((character, i)=>{
-      return i !==index;
-    });
-    setCharacters(updated);
+    const toDelete = characters[index];
+    const id = toDelete?.id;
+    if(!id){
+      console.error("Can't delete");
+      return;
+    }
+    fetch(`http://localhost:8000/users/${id}`, {method: "DELETE"})
+      .then(res =>{
+        if(res.status === 204){
+          setCharacters(prev => prev.filter((_, i) => i !== index));
+        }
+      })
+      .catch(err => console.error("Error deleting:", err))
   }
   function updateList(person){
-    setCharacters([...characters, person]);
+    fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(person),
+    })
+    .then(res => res.json())
+    .then(newUser => setCharacters([...characters, newUser]))
+    .catch(err => console.error(err));
+  }
+  function postUser(person){
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
   }
   return(
     <div className ="container">
